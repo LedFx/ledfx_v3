d4d6204a956e152f99e4f1e63c4d3720ca3aadaf
Matthew Bowley <m.bowley98@gmail.com>
2022-01-04 12:42:44 +0000

    Add delete method to api

--- api/api.go
+++ api/api.go
@@ -17,7 +17,7 @@ func SetHeader(w http.ResponseWriter) {
 	headers.Add("Vary", "Access-Control-Request-Method")
 	headers.Add("Vary", "Access-Control-Request-Headers")
 	headers.Add("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token")
-	headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
+	headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 }
 
 type RespConfig struct {

a019427031eb4c0a8bd0948254d2937518a39b8b
carterpeel <carterpeel@gmail.com>
2022-01-25 14:35:33 -0500

    Added comments on testfile

--- integrations/integrationTests/audioBridge_test.go
+++ integrations/integrationTests/audioBridge_test.go
@@ -7,19 +7,31 @@ import (
 )
 
 func TestAudioBridge(t *testing.T) {
+	// srcConf is the config for the endpoint from which
+	// audio will be ingested, processed, converted, and
+	// redistributed.
 	srcConf := audiobridge.EndpointConfig{
-		Type:    audiobridge.DeviceTypeAirPlay,
-		Name:    "LedFX-Input-Test",
-		Verbose: true,
+		Type:    audiobridge.DeviceTypeAirPlay, // Spin up an AirPlay server
+		Name:    "LedFX-Input-Test",            // It will be advertised as "LedFX-Input-Test"
+		Verbose: true,                          // Enable verbosity because this is a test package.
 	}
+
 	dstConf := audiobridge.EndpointConfig{
-		Type:    audiobridge.DeviceTypeAirPlay,
-		Name:    "(?i)Output$",
-		Verbose: true,
+		Type: audiobridge.DeviceTypeAirPlay, // Connect to an airplay server
+
+		// We will connect to any AirPlay server that contains the string "Output" (case-insensitive)
+		Name: "(?i)Output$", // Regular expressions are required here.
+
+		Verbose: true, // Enable verbosity because this is a test package.
 	}
+
+	// Initialize the bridge. This will start everything, too.
 	bridge, err := audiobridge.NewBridge(srcConf, dstConf, io.Discard)
 	if err != nil {
 		t.Fatalf("error creating new audio bridge: %v\n", err)
 	}
+
+	// Wait until the bridge loop stops. This can be called with Stop()
 	bridge.Wait()
+
 }

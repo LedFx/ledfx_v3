87f1ad9bfcac48c591aa09ce91dfebbb61c6828d
carterpeel <carterpeel@gmail.com>
2022-02-17 12:42:32 -0500

    Added ip/port flags

--- examples/apicontrol/main.go
+++ examples/apicontrol/main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"flag"
 	"ledfx/audio"
 	"log"
+)
+
+func init() {
+	flag.IntVar(&port, "port", 8080, "The port the API server will listen on")
+	flag.StringVar(&ip, "ip", "0.0.0.0", "The IP address the API server will listen on")
+}
+
+var (
+	port int
+	ip   string
 )
 
 func main() {
@@ -13,5 +24,5 @@ func main() {
 		log.Panicf("Error initializing API server: %v\n", err)
 	}
 
-	log.Panicf("Error starting API server: %v\n", server.Serve("127.0.0.1", 8080))
+	log.Panicf("Error starting API server: %v\n", server.Serve(ip, port))
 }

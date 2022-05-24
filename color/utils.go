0d3577337e83fb9dbc4ad479d62ee2d980ecc7bc
Matthew Bowley <m.bowley98@gmail.com>
2022-05-24 23:38:33 +0100

    Colour utilities

--- color/utils.go
+++ color/utils.go
@@ -1,33 +1,30 @@
 package color
 
-// Desaturates a color
-// 1: color remains unchanged, 0: color is fully desaturated
-func Desaturate(c *Color, s float64) {
-	_, max := minMaxArray(c[:])
-	for _, value := range c {
-		diff := max - value
-		value += diff * s
+import "math"
+
+// Shift the hue of pixels
+func HueShiftPixels(p Pixels, hs float64) {
+	for _, color := range p {
+		h := color[0]
+		h += hs
+		color[0] = math.Mod(h, 1)
 	}
 }
 
-// Apply desaturation to pixels
-func DesaturatePixels(p *Pixels, s float64) {
-	for _, color := range *p {
-		Desaturate(&color, s)
+// Apply desaturation to pixels (they're fully saturated by default)
+func DesaturatePixels(p Pixels, ds float64) {
+	for _, color := range p {
+		color[1] *= ds
 	}
 }
 
-// TODO rename and move this somewhere more sensible
-func minMaxArray(array []float64) (float64, float64) {
-	var max float64 = array[0]
-	var min float64 = array[0]
-	for _, value := range array {
-		if max < value {
-			max = value
-		}
-		if min > value {
-			min = value
-		}
+// Darken pixels (they're full brightness by default)
+func DarkenPixels(p Pixels, ds float64) {
+	for _, color := range p {
+		color[2] *= ds
 	}
-	return min, max
+}
+
+func ToRGB(p Pixels) {
+
 }

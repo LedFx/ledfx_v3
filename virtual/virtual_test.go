1b7a3770d82a6369879285666e52a4d32643c85b
Matthew Bowley <m.bowley98@gmail.com>
2022-05-27 23:52:23 +0100

    First working demo, single packet.

--- virtual/virtual_test.go
+++ virtual/virtual_test.go
@@ -1,1 +1,45 @@
 package virtual
+
+import (
+	"ledfx/color"
+	"ledfx/device"
+	"ledfx/effect"
+	"testing"
+)
+
+func TestVirtual(t *testing.T) {
+
+	bdc := device.BaseDeviceConfig{
+		PixelCount: 64,
+		Name:       "Spotlight",
+	}
+	udpc := device.UDPConfig{
+		NetworkerConfig: device.NetworkerConfig{
+			IP:   "192.168.0.72",
+			Port: 21324,
+		},
+		Protocol: device.DRGB,
+		Timeout:  60,
+	}
+	d, _, err := device.New("udp", bdc, udpc)
+	if err != nil {
+		t.Error(err)
+	}
+	err = d.Connect()
+	if err != nil {
+		t.Error(err)
+	}
+
+	e, _, err := effect.New("energy", bdc.PixelCount, nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	p := make(color.Pixels, bdc.PixelCount)
+
+	e.Render(p)
+	err = d.Send(p)
+	if err != nil {
+		t.Error(err)
+	}
+}

8e52c016ca499c47cbc1cbd52950a77393da1188
Matthew Bowley <m.bowley98@gmail.com>
2022-06-07 23:58:01 +0100

    update expfilter

--- math_utils/exp_filter.go
+++ math_utils/exp_filter.go
@@ -5,13 +5,21 @@ Exponential filters smooth the rise and fall of values
 */
 
 // Exponential filter for a single value
-type ExpFilter struct {
+type expFilter struct {
 	alphaDecay float64
 	alphaRise  float64
 	Value      float64
 }
 
-func (e *ExpFilter) Update(value float64) {
+func NewExpFilter() *expFilter {
+	return &expFilter{
+		alphaDecay: 0,
+		alphaRise:  0,
+		Value:      0,
+	}
+}
+
+func (e *expFilter) Update(value float64) {
 	var alpha float64
 	if value > e.Value {
 		alpha = e.alphaRise
@@ -22,12 +30,19 @@ func (e *ExpFilter) Update(value float64) {
 }
 
 // Exponential filter for a slice
-type ExpFilterSlice struct {
-	ExpFilter
+type expFilterSlice struct {
+	expFilter
 	Value []float64
 }
 
-func (e *ExpFilterSlice) Update(value []float64) {
+func NewExpFilterSlice(size int) *expFilterSlice {
+	return &expFilterSlice{
+		expFilter: expFilter{},
+		Value:     make([]float64, size),
+	}
+}
+
+func (e *expFilterSlice) Update(value []float64) {
 	var alpha float64
 	for i := range value {
 		if value[i] > e.Value[i] {

e40f2d8b7dcad752033940dee82325a21b1f40bc
Matthew Bowley <m.bowley98@gmail.com>
2022-06-08 00:37:31 +0100

    analyser test and bench

--- audio/analysis_test.go
+++ audio/analysis_test.go
@@ -4,10 +4,18 @@ import (
 	"testing"
 )
 
-var testAudio Buffer = make(Buffer, framesPerBuffer*4)
+var testAudio Buffer = make(Buffer, framesPerBuffer*2)
+
+func TestAnalysis(t *testing.T) {
+	Analyzer.NewMelbank("totally_valid_id", Mono, uint(20), uint(20000))
+	Analyzer.BufferCallback(testAudio)
+	Analyzer.Cleanup()
+}
 
 func BenchmarkAnalysis(t *testing.B) {
+	Analyzer.NewMelbank("totally_valid_id", Mono, uint(20), uint(20000))
 	for i := 0; i < t.N; i++ {
 		Analyzer.BufferCallback(testAudio)
 	}
+	Analyzer.Cleanup()
 }

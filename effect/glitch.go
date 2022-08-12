67554dea335238faeb9606920ea304e8ba14aff4
Matthew Bowley <m.bowley98@gmail.com>
2022-08-12 14:26:13 +0200

    glitch effect
    Co-authored-by: Fredrik Bränström <branstrom@gmail.com>

---
+++ effect/glitch.go
@@ -0,0 +1,45 @@
+package effect
+
+import (
+	"ledfx/color"
+	"math"
+)
+
+/*
+Audio reactive port of PixelBlaze effect "Glitch Bands"
+Credit to Ben Henke for original concept & fantastic LED art
+https://electromage.com/patterns
+*/
+
+type Glitch struct{}
+
+// movement speed based on beat
+// modify saturation by vocals/mids
+// make the bars/legs grow and shrink
+// modulate t1 with highs
+
+// Apply new pixels to an existing pixel array.
+func (e *Glitch) assembleFrame(base *Effect, p color.Pixels) {
+
+	t1 := base.time(0.1) * math.Pi * 2
+	t2 := base.time(0.1)
+	t3 := base.time(0.5)
+	t4 := base.time(0.2) * math.Pi * 2
+	t5 := base.time(0.05)
+	t6 := base.time(0.02)
+
+	for i := 0; i < len(p); i++ {
+		fi := float64(i)
+		m := (0.3 + base.triangle(t2)*0.2)
+		h := math.Sin(t1) + math.Mod(((fi-base.pixelScaler/2)/base.pixelScaler)*(base.triangle(t3)*10+4*math.Sin(t4)), m)
+		s1 := base.triangle(math.Mod(t5+fi/base.pixelScaler*5, 1))
+		s1 = math.Pow(s1, 2)
+		s2 := base.triangle(math.Mod(t6-(fi-base.pixelScaler)/base.pixelScaler, 1))
+		s2 = math.Pow(s2, 4)
+		s := 1 - base.triangle(s1*s2)
+
+		p[i][0] = h
+		p[i][1] = s
+		p[i][2] = 1
+	}
+}
